52dbab5af3de547356c2ff0164f2ec65e8f10e98
ptran779 <phong6900user@gmail.com>
2023-01-28 19:24:37 -0600

    update structure

--- main2.cpp
+++ main2.cpp
@@ -6,11 +6,12 @@
 using namespace std;
 
 class team{
-protected:
+public:
     unsigned int capacity;
     vector<int> preferred;
     vector<int> tolerated;
     unordered_set<int> noway;
+    int rank_score=0;
 
 public:
     team() : capacity(0) {};
@@ -33,17 +34,19 @@ public:
       }
       this->noway = std::move(nowayVec);
     }
-    vector<int> * change_prefrence(){
-      return & preferred;
-    }
-    vector<int> * change_tolerated(){
-      return & tolerated;
-    }
-    unordered_set<int> * change_noway(){
-      return & noway;
-    }
 };
 
+vector<int> rankTeam(vector<team> &allTeam){
+  vector<int> out(allTeam.size(), 0);
+  for (int i=0; i<allTeam.size(); i++){  // loop team
+    for (int j : allTeam[i].preferred){  // loop team preference
+      if ((allTeam[j-1].noway).find(i+1) == (allTeam[j-1].noway).end()) {// check for noway conflict
+        out[j-1]++;
+      }
+    }
+  }
+  return out;
+}
 
 
 int main(){
@@ -51,22 +54,19 @@ int main(){
   vector<team> allTeam;
   allTeam.push_back(team(1, 22, {2,4,6,11}, {3,8,10}, 11));
   allTeam.push_back(team(2, 45, {1,3,5}, {6,7,11}, 11));
-  allTeam.push_back(team(3, 34, {2,4,6,11}, {3,8,10}, 11));
-  allTeam.push_back(team(4, 51, {1,2,11}, {7}, 11));
-  allTeam.push_back(team(5, 11, {10,1,3}, {1,3}, 11));
+  allTeam.push_back(team(3, 34, {1,2,11}, {7}, 11));
+  allTeam.push_back(team(4, 51, {10}, {1,3}, 11));
+  allTeam.push_back(team(5, 11, {1,2,3,4}, {9,10,11}, 11));
   allTeam.push_back(team(6, 37, {7,10}, {1,8}, 11));
   allTeam.push_back(team(7, 42, {1,2,3,4,5,6}, {10,11}, 11));
-  allTeam.push_back(team(8, 16, {1,10}, {3,8,10}, 11));
-  allTeam.push_back(team(9, 29, {1,5}, {3,8,10}, 11));
+  allTeam.push_back(team(8, 16, {1,10}, {2,4,11}, 11));
+  allTeam.push_back(team(9, 29, {1,5}, {2,10}, 11));
   allTeam.push_back(team(10, 56, {2,6,7,11}, {4,5,8}, 11));
   allTeam.push_back(team(11, 49, {1,4,5}, {2,3,6,7,9,10}, 11));
 
-  cout<<"hello uhack"<<endl;
-  // floor A
-  for (team i : allTeam){
-    for(int j: *i.change_noway()) cout << j << " ";
-    cout << endl;
-  }
+  vector<int> ranked = rankTeam(allTeam);
+  for(int i:ranked) cout << i << " ";
+  cout << endl;
   return 0;
 }
 
